f057caeef17a7bb6206ddbc8e490b20adfe41144
wifidu <875147715@qq.com>
2019-11-08 20:25:45 +0800

    登录和注册

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,26 +1,50 @@
-<nav class="navbar navbar-expand-lg navbar-light  navbar-static-top">
-  <div class="container">
-    <a class="navbar-brand" href="{{ url('/') }}">
-      wffl
+<nav class="navbar navbar-expand-lg navbar-light navbar-static-top">
+<div class="container">
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
+    wffl
     </a>
-  </div>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" 
-    data-target="#navbarSupportedContent">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+    </button>
 
-  <div id="navbarSupportedContent" class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <!-- Left Side Of Navbar -->
     <ul class="navbar-nav mr-auto">
 
     </ul>
 
-    <form class="form-inline my-2 my-lg-0 mr-4">
+    <form class="form-inline mr-5 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+
+    <!-- Right Side Of Navbar -->
     <ul class="navbar-nav navbar-right">
-      <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-      <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="">个人中心</a>
+            <a class="dropdown-item" href="">编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST">
+                {{ csrf_field() }}
+                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+            </a>
+            </div>
+        </li>
+        @endguest
     </ul>
-  </div>
+    </div>
+</div>
 </nav>
