f5523b24540d93e3f421f210add870271d9cd057
wifidu <875147715@qq.com>
2020-01-01 12:05:57 +0800

    性能优化，修复N+1问题

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -11,7 +11,7 @@ class CategoriesController extends Controller
     public function show(Category $category)
     {
         // 读取分类ID关联的话题，并按每20条分页
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = Topic::where('category_id', $category->id)->with('user', 'category')->paginate(20);
         // 传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category'));
     }
