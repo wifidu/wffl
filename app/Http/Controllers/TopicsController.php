c99d08f7b105af6624a081d430a16e1ba2958e2a
wifidu <875147715@qq.com>
2020-01-01 12:28:38 +0800

    话题排序，利用本地作用域，在模型中定义函数，控制器直接调用，只要在函数名前面加上scope即可

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,9 +14,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index()
+    public function index(Request $request,  Topic $topic)
     {
-        $topics = Topic::with('user', 'category')->paginate();
+        $topics = $topic->withOrder($request->order)
+                        ->with('user', 'category')
+                        ->paginate();
         return view('topics.index', compact('topics'));
     }
 
