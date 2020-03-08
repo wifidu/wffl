1fafadb505b0ba998ae21ea633d2c28682f969e2
wifidu <875147715@qq.com>
2020-03-08 14:55:31 +0800

    用户可以删除话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -64,7 +64,7 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
 
-        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+        return redirect()->route('topics.index')->with('success', 'Deleted successfully.');
     }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
