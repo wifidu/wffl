77022422ff141ab8eec1b46466bbe7f2140045c4
wifidu <875147715@qq.com>
2020-03-08 14:19:20 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title', 'wffl') - wffl</title>
+  <meta name="description" content="@yield('description', 'weifan\'s first blog')" />
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
