ac788d7a5c038a04b34cbe1a98ac6bb2868a0e9e
wifidu <875147715@qq.com>
2020-03-31 13:56:11 +0800

    删除话题

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -31,4 +31,13 @@ class TopicsController extends Controller
 
         return new TopicResource($topic);
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+
+        return response(null, 204);
+    }
 }
