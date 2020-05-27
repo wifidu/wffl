cb5c81fabe051b9ad17e90f71ac5684622f269e8
wifidu <875147715@qq.com>
2020-05-27 22:59:18 +0800

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
+});
